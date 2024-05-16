f160c3d918eb42fed616e6e8146844b05dc068d7
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-05-16 16:18:39 +0200

    add Snack5.java exercise description

--- src/org/exercise/Snack5.java
+++ src/org/exercise/Snack5.java
@@ -1,6 +1,12 @@
 package org.exercise;
 import java.util.Scanner;
 
+/* Snack5
+*  Data una stringa in input mostrare a video quanti caratteri alfabetici contiene,
+*  quanti numeri e quanti simboli non alfanumerici.
+*  Continuare a chiedere una nuova stringa finchè non si inserisce 0.
+* */
+
 public class Snack5 {
     public static void main(String[] args) {
         // Oggetto scanner
