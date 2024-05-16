b0bcfef70e65248f2ae4315d5c6f36b4a2d1d81a
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-05-16 15:41:29 +0200

    add Snack4.java

---
+++ src/org/exercise/Snack4.java
@@ -0,0 +1,41 @@
+package org.exercise;
+import java.util.Scanner;
+
+/* Snack4
+ * Data in input una stringa verificare se è palindroma.
+ * */
+
+public class Snack4 {
+    public static void main(String[] args) {
+        // Oggetto scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Input utente
+        System.out.print("Inserisci una frase: ");
+        // Acquisisco l'input e lo converto in lettere minuscole
+        String word = scanner.nextLine().toLowerCase();
+        scanner.close();
+
+        // Flag per validazione stringa
+        boolean isPalindrome = false;
+
+        // Validazione stringa (se è o meno palindroma)
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+                // Se i caratteri corrispondenti non sono uguali,w non è palindroma
+                isPalindrome = false;
+                break;
+            } else {
+                // Se i caratteri corrispondenti sono uguali, è palidroma
+                isPalindrome = true;
+            }
+        }
+
+        // Stampo il risultato
+        if (isPalindrome) {
+            System.out.println(word + " è una parola palindroma.");
+        } else {
+            System.out.println(word + " non è una parola palindroma.");
+        }
+    }
+}
